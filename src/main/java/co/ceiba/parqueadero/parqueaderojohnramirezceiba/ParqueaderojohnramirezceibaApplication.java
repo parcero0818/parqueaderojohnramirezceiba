67f0f1d3b905522d47fa88479bcb04e0aad2a855
John Edward Ramírez Arango - Ceiba Software <john.ramirez@ceiba.com.co>
2018-06-18 10:52:44 -0500

    primer commit

---
+++ src/main/java/co/ceiba/parqueadero/parqueaderojohnramirezceiba/ParqueaderojohnramirezceibaApplication.java
@@ -0,0 +1,12 @@
+package co.ceiba.parqueadero.parqueaderojohnramirezceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderojohnramirezceibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderojohnramirezceibaApplication.class, args);
+	}
+}
